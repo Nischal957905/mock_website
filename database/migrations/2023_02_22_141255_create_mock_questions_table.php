5944883446338dfcb19ddbda595296177d0c737f
Nischal957905 <nency.gole@gmail.com>
2023-02-23 10:44:15 +0545

    First-Commit
    Initial Commit

---
+++ database/migrations/2023_02_22_141255_create_mock_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mock_questions', function (Blueprint $table) {
+            $table->unsignedBigInteger('mock_id');
+            $table->unsignedBigInteger('question_id');
+            $table->foreign('mock_id')->references('id')->on('mocks');
+            $table->foreign('question_id')->references('id')->on('questions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mock_questions');
+    }
+};
